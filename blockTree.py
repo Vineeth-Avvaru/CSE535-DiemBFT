cf825c3fd543db3362a33527516bb5a0ffd6a7eb
Swaroop Bugatha <jyothiswaroop6050@gmail.com>
2021-10-16 20:42:29 -0400

    Updated ds

--- blockTree.py
+++ blockTree.py
@@ -97,16 +97,16 @@ class TC:
         self.tmo_signatures = tmo_signatures
 
 class TimeoutMsg:
-    def __init__(self, tmo_info, last_round_tc, high_commit_tc):
+    def __init__(self, tmo_info, last_round_tc, high_commit_qc):
         self.tmo_info = tmo_info
         self.last_round_tc = last_round_tc
-        self.high_commit_tc = high_commit_tc
+        self.high_commit_qc = high_commit_qc
 
 class ProposalMsg:
-    def __init__(self, block, last_round_tc, high_commit_tc):
+    def __init__(self, block, last_round_tc, high_commit_qc):
         self.block = block
         self.last_round_tc = last_round_tc
-        self.high_commit_tc = high_commit_tc
+        self.high_commit_qc = high_commit_qc
         # signature <- sign(block_id)
 
 class PendingBlockTree:
