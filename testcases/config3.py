a6669355df513c2116860802e0a879e51180037e
Prajeeth Emanuel <daedra.emanuel.779@gmail.com>
2021-10-17 23:59:18 -0400

    adding new test cases

---
+++ testcases/config3.py
@@ -0,0 +1,14 @@
+test_config =  [
+  {
+    "nClients": 7,
+    "nReplicas": 7,
+    "num_transactions": 10,
+    "seed": "consistency",
+    "f": 1,
+    "timeout": 1,
+    "window_size": 1,
+    "exclude_size": 1,
+    "test_case_details": "Test Case with higher transaction messages and higher replicas",
+    "delta": 0.25
+  }
+]
---
+++ testcases/config4.py
@@ -0,0 +1,14 @@
+test_config =  [
+  {
+    "nClients": 14,
+    "nReplicas": 14,
+    "num_transactions": 2,
+    "seed": "consistency",
+    "f": 1,
+    "timeout": 1,
+    "window_size": 1,
+    "exclude_size": 1,
+    "test_case_details": "Test Case with higher transaction messages and higher replicas",
+    "delta": 0.25
+  }
+]
